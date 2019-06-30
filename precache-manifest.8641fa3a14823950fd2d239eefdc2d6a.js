0ce7aba64a4c0c5239d2a8ac24dc81eed59e6cd4
SamSD <ss15060@my.bristol.ac.uk>
2019-06-30 13:17:30 +0100

    Updates

---
+++ precache-manifest.8641fa3a14823950fd2d239eefdc2d6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c88011cbbe75d1f7db98a6463e6df5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a54142ee13fc1e331bbe",
+    "url": "/static/css/main.4afa2deb.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "a54142ee13fc1e331bbe",
+    "url": "/static/js/main.79954032.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
