dc1b0c45a32f6b8f21dafff405b24a24ebe060c5
SamSD <ss15060@my.bristol.ac.uk>
2019-08-09 20:53:35 +0100

    Updates

--- precache-manifest.bc27127decfd3593be7fae7a3a19ad5d.js
+++ precache-manifest.bc27127decfd3593be7fae7a3a19ad5d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cde54bdf32ec1d98cf94383491892e27",
+    "revision": "fceb44c3c67612a1ab009b467d61548f",
     "url": "/index.html"
   },
   {
-    "revision": "536eda2ab4126ca770fc",
+    "revision": "a5fb67d22e4bddacfab9",
     "url": "/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "3e05041e2479e3019925",
+    "revision": "25d905241212d129012b",
     "url": "/static/css/main.22cf8afe.chunk.css"
   },
   {
-    "revision": "536eda2ab4126ca770fc",
-    "url": "/static/js/2.4a69e599.chunk.js"
+    "revision": "a5fb67d22e4bddacfab9",
+    "url": "/static/js/2.d6d9a38c.chunk.js"
   },
   {
-    "revision": "3e05041e2479e3019925",
-    "url": "/static/js/main.c845e809.chunk.js"
+    "revision": "25d905241212d129012b",
+    "url": "/static/js/main.19cfb6bc.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30fa6929fe7794f7ba8fb557410fc777.js"
+  "/precache-manifest.bc27127decfd3593be7fae7a3a19ad5d.js"
 );
 
 self.addEventListener('message', (event) => {
